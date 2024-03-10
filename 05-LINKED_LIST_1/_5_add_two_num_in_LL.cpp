353a9fbfebb9ce6291eaa3431e9dfda250117fe3
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-03-10 11:18:08 +0530

    Update _5_add_two_num_in_LL.cpp

--- 05-LINKED_LIST_1/_5_add_two_num_in_LL.cpp
+++ 05-LINKED_LIST_1/_5_add_two_num_in_LL.cpp
@@ -64,28 +64,27 @@ public:
 
 
         //Reducing the above while loops 
-        // ListNode *dummy = new ListNode(); 
-        // ListNode *temp = dummy; 
-        // int carry = 0;
-        // while( (l1 != NULL || l2 != NULL) || carry) {
-        //     int sum = 0; 
-        //     if(l1 != NULL) {
-        //         sum += l1->val; 
-        //         l1 = l1 -> next; 
-        //     }
+        ListNode *dummy = new ListNode(); 
+        ListNode *temp = dummy; 
+        int carry = 0;
+        while( (l1 != NULL || l2 != NULL) || carry) {
+            int sum = 0; 
+            if(l1 != NULL) {
+                sum += l1->val; 
+                l1 = l1 -> next; 
+            }
             
-        //     if(l2 != NULL) {
-        //         sum += l2 -> val; 
-        //         l2 = l2 -> next; 
-        //     }
+            if(l2 != NULL) {
+                sum += l2 -> val; 
+                l2 = l2 -> next; 
+            }
             
-        //     sum += carry; 
-        //     carry = sum / 10; 
-        //     ListNode *node = new ListNode(sum % 10); 
-        //     temp -> next = node; 
-        //     temp = temp -> next; 
-        // }
-        // return dummy -> next; 
-
+            sum += carry; 
+            carry = sum / 10; 
+            ListNode *node = new ListNode(sum % 10); 
+            temp -> next = node; 
+            temp = temp -> next; 
+        }
+        return dummy -> next; 
     }
 };
