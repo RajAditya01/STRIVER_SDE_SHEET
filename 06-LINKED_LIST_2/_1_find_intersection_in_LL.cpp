0f03da0cf52f2aa6da97b4318a7d72ab6fd37dc2
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-03-11 23:33:49 +0530

    Update _1_find_intersection_in_LL.cpp

--- 06-LINKED_LIST_2/_1_find_intersection_in_LL.cpp
+++ 06-LINKED_LIST_2/_1_find_intersection_in_LL.cpp
@@ -17,71 +17,71 @@ class Solution {
 public:
 
     //Using Brute force Approach
-    // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    //     ListNode* l1=headA;
-    //     ListNode* l2=headB;
-    //     while(l1){
-    //         while(l2){
-    //             if(l1==l2){
-    //                 return l1;
-    //             }
-    //             l2=l2->next;
-    //         }
-    //         l2=headB;
-    //         l1=l1->next;
-    //     }
-    //     return NULL;
-    // }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* l1=headA;
+        ListNode* l2=headB;
+        while(l1){
+            while(l2){
+                if(l1==l2){
+                    return l1;
+                }
+                l2=l2->next;
+            }
+            l2=headB;
+            l1=l1->next;
+        }
+        return NULL;
+    }
 
 
     //Using hashMap
-    // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
-    //     unordered_set<ListNode*>s;
-    //     ListNode* l1=headA;
-    //     ListNode* l2=headB;
-    //     while(l1){
-    //         s.insert(l1);
-    //         l1=l1->next;
-    //     }
-    //     while(l2){
-    //         if(s.find(l2)!=s.end()){
-    //             return l2;
-    //         }
-    //         l2=l2->next;
-    //     }
-    //     return NULL;
-    // }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
+        unordered_set<ListNode*>s;
+        ListNode* l1=headA;
+        ListNode* l2=headB;
+        while(l1){
+            s.insert(l1);
+            l1=l1->next;
+        }
+        while(l2){
+            if(s.find(l2)!=s.end()){
+                return l2;
+            }
+            l2=l2->next;
+        }
+        return NULL;
+    }
 
 
     //using calculating the difference
-    // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
-    //     ListNode* l1=headA;
-    //     ListNode* l2=headB;
-    //     int d1=0,d2=0;
-    //     while(l1 || l2){
-    //         if(l1){
-    //             d1++;
-    //             l1=l1->next;
-    //         }
-    //         if(l2){
-    //             d2++;
-    //             l2=l2->next;
-    //         }
-    //     }
-    //     l1=headA;
-    //     l2=headB;
-    //     int diff=d1-d2;
-    //     if(diff < 0) 
-    //         while(diff++ != 0) l2 = l2->next; 
-    //     else while(diff-- != 0) l1 = l1->next;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
+        ListNode* l1=headA;
+        ListNode* l2=headB;
+        int d1=0,d2=0;
+        while(l1 || l2){
+            if(l1){
+                d1++;
+                l1=l1->next;
+            }
+            if(l2){
+                d2++;
+                l2=l2->next;
+            }
+        }
+        l1=headA;
+        l2=headB;
+        int diff=d1-d2;
+        if(diff < 0) 
+            while(diff++ != 0) l2 = l2->next; 
+        else while(diff-- != 0) l1 = l1->next;
 
-    //     while(l1 != NULL) {
-    //         if(l1 == l2) return l1;
-    //         l2 = l2->next;
-    //         l1 = l1->next;
-    //     }
-    //     return l1;
-    // }
+        while(l1 != NULL) {
+            if(l1 == l2) return l1;
+            l2 = l2->next;
+            l1 = l1->next;
+        }
+        return l1;
+    }
 
 
     //optimised solution
